72bb0e535aa593869a94b405cf9673a75bf98758
SzaboTamas <tomika1097@gmail.com>
2021-02-23 22:42:39 +0100

    Create main.h and main.cpp

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by SzaboTamas on 2021. 02. 23..
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by SzaboTamas on 2021. 02. 23..
+//
+
+#ifndef SZABOTAMAS_GITGYAKORLAS_MAIN_H
+#define SZABOTAMAS_GITGYAKORLAS_MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //SZABOTAMAS_GITGYAKORLAS_MAIN_H
